da0f73d396154240db0c079160bbc286ae1d18eb
Nikita <lavro.nikita123@gmail.com>
2023-02-18 20:56:57 +0200

    create a simple server

--- index.js
+++ index.js
@@ -1,28 +1,10 @@
-const fs = require("fs");
+const http = require("http");
 
-//Blocking,sync way
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+const server = http.createServer((req, res) => {
+  console.log(req);
+  res.end("Hello from the server!");
+});
 
-const textOut = `This is what we know about the avocado ${textIn}\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File written");
-
-//Non-blocking, async way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
-      console.log(data3);
-
-      fs.writeFile(
-        "./txt/final.txt",
-        `${data2}\n${data3}`,
-        "utf-8",
-        (error) => {
-          console.log("File has been written");
-        }
-      );
-    });
-  });
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
 });
