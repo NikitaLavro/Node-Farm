b4e8b1938c0e635d656f884febaa8348d37df1dc
Nikita <lavro.nikita123@gmail.com>
2023-02-18 21:47:05 +0200

    /api route json sending

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const http = require("http");
+const fs = require("fs");
 const url = require("url");
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
@@ -8,6 +12,11 @@ const server = http.createServer((req, res) => {
     res.end("This is the OVERVIEW");
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-Type": "text/html",
