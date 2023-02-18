a89a9512d2f946d2e29d9610286432da9bbd82c2
Nikita <lavro.nikita123@gmail.com>
2023-02-18 22:55:59 +0200

    import html files and routing comments

--- index.js
+++ index.js
@@ -3,20 +3,41 @@ const fs = require("fs");
 const url = require("url");
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const templateOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const templateCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+
+const templateProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  //Overview page
   if (pathName === "/" || pathName === "/overview") {
     res.end("This is the OVERVIEW");
+
+    //Product page
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
+
+    //Api page
   } else if (pathName === "/api") {
     res.writeHead(200, {
       "Content-Type": "application/json",
     });
     res.end(data);
+
+    //Not found page
   } else {
     res.writeHead(404, {
       "Content-Type": "text/html",
